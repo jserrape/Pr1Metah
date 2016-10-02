18e1c9c14f761ed27da3e4d4c9fa1c7aa634a065
xenahort <jcsp0003@red.ujaen.es>
2016-10-02 17:25:49 +0200

    -Se ha añadido el vector ratio -Se han borrado comentarios

--- Pr1Metah/src/pr1metah/Pr1Metah.java
+++ Pr1Metah/src/pr1metah/Pr1Metah.java
@@ -20,9 +20,36 @@ import java.io.IOException;
 public class Pr1Metah {
 
     static int cubre[];
+    static float ratio[];
     static int matriz[][];
     static int x, y;
 
+    public static void mostrarMatrizYVector() {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n\n\n\n");
+        for (int i = 0; i < x; i++) {
+            System.out.print(cubre[i] + ".0 ");
+        }
+        System.out.print("\n\n");
+        for (int i = 0; i < x; i++) {
+            System.out.print(ratio[i] + " ");
+        }
+        System.out.print("\n\n\n\n");
+    }
+
+    public static void rellenarRatio(){
+        ratio = new float[x];
+        cubre[0]=0;
+        for (int i = 1; i < x; i++) {
+            ratio[i]=cubre[i]/matriz[0][i];
+        }
+    } 
+    
     public static void leerFichero(String fich) throws FicheroNoEncontrado {
         if (!(new File(fich)).exists()) {
             throw new FicheroNoEncontrado("Fichero no encontrado \n");
@@ -37,17 +64,15 @@ public class Pr1Metah {
             String texto;
             String[] datos;
             System.out.print("Fichero abierto correctamente\n");
-            //System.out.print("Vamoh a leer\n");
             texto = br.readLine();
             datos = texto.split(" ");
-            //System.out.print(texto + "\n");
             y = Integer.parseInt(datos[1]) + 1;
             x = Integer.parseInt(datos[2]) + 1;
 
             matriz = new int[y][x];
             cubre = new int[x];
 
-            for (int i = 0; i < x; i++) {   //Puedo meter esto cuando meto los costes para optimizar
+            for (int i = 0; i < x; i++) {
                 cubre[i] = 0;
             }
 
@@ -57,50 +82,23 @@ public class Pr1Metah {
                 }
             }
 
-            /*
-            for (int i = 0; i < x; i++) {
-                matriz[0][i] = 0;
-            }*/
             for (int j = 0; j < y; j++) {
                 matriz[j][0] = -1;
             }
-
-            /*
-            for (int i = 0; i < y; i++) {
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-            //matriz[0][0]=0; //BORRAR MAS ADELANTE, ESTA PARA QUE AL IMPRIMIR LA MATRIZ QUEDE RECTA
             int comisariasV = 1;
-            //System.out.print("Y vale: " + x + "  comisariasV vale: " + comisariasV + " \n");
             while (x != comisariasV) {
                 texto = br.readLine();
                 datos = texto.split(" ");
                 for (int i = 1; i < datos.length; i++) {
                     matriz[0][comisariasV] = Integer.parseInt(datos[i]);
                     ++comisariasV;
-                    //System.out.print("Y vale: " + x + "  comisariasV vale: " + comisariasV + " \n");
                 }
             }
-            //System.out.print(texto = br.readLine()+"  SOBROOOOOOOOO\n"); //<---------------------- SOBRA URGENTEMENTE
-
-            /*
-            for (int i = 0; i < y; i++) {
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-            //System.out.print("X vale: " + y + " \n");
             int cont;
             for (int i = 1; i < y; i++) {
-                //System.out.print("Leyendo las colindancias del territorio " + i + " \n");
                 texto = br.readLine();
                 datos = texto.split(" ");
                 cont = Integer.parseInt(datos[1]);
-                //System.out.print("El territorio " + i + " tiene " + cont + " colindancias. \n");
                 while (cont != 0) {
                     texto = br.readLine();
                     datos = texto.split(" ");
@@ -111,21 +109,6 @@ public class Pr1Metah {
                     }
                 }
             }
-
-            //System.out.print(Arrays.toString(datos)+"aaaaaaaaaaa \n");
-            /*
-            for (int i = 0; i < y; i++) {    //SOBRA
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-
- /*
-            while ((texto = br.readLine()) != null) {
-                System.out.print(texto + "\n");
-            }
-             */
         } catch (IOException | NumberFormatException e) {
         } finally {
             try {
@@ -141,19 +124,12 @@ public class Pr1Metah {
     public static void main(String[] args) {
         String errores = "";
         try {
-            leerFichero("scpe1.txt");
-            for (int i = 0; i < y; i++) {
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }
-
-            System.out.print("\n\n\n\n");
-
-            for (int i = 0; i < x; i++) {
-                System.out.print(cubre[i] + " ");
-            }
+            
+            leerFichero("scpnrf1.txt");
+            rellenarRatio();
+            mostrarMatrizYVector();
+            
+            
         } catch (FicheroNoEncontrado error) {
             errores = error.getMessage();
         }
