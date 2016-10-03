b3642e365e79f3e1a4eb13055b40567424bc065c
Juanca <Juanca@10.82.115.159>
2016-10-03 10:54:52 +0200

    - Eliminación de código innecesario

--- Pr1Metah/src/pr1metah/Pr1Metah.java
+++ Pr1Metah/src/pr1metah/Pr1Metah.java
@@ -27,6 +27,7 @@ public class Pr1Metah {
         if (!(new File(fich)).exists()) {
             throw new FicheroNoEncontrado("Fichero no encontrado \n");
         }
+        
         File archivo;
         FileReader fr = null;
         BufferedReader br;
@@ -37,10 +38,10 @@ public class Pr1Metah {
             String texto;
             String[] datos;
             System.out.print("Fichero abierto correctamente\n");
-            //System.out.print("Vamoh a leer\n");
+
             texto = br.readLine();
             datos = texto.split(" ");
-            //System.out.print(texto + "\n");
+
             y = Integer.parseInt(datos[1]) + 1;
             x = Integer.parseInt(datos[2]) + 1;
 
@@ -57,50 +58,25 @@ public class Pr1Metah {
                 }
             }
 
-            /*
-            for (int i = 0; i < x; i++) {
-                matriz[0][i] = 0;
-            }*/
             for (int j = 0; j < y; j++) {
                 matriz[j][0] = -1;
             }
 
-            /*
-            for (int i = 0; i < y; i++) {
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-            //matriz[0][0]=0; //BORRAR MAS ADELANTE, ESTA PARA QUE AL IMPRIMIR LA MATRIZ QUEDE RECTA
             int comisariasV = 1;
-            //System.out.print("Y vale: " + x + "  comisariasV vale: " + comisariasV + " \n");
             while (x != comisariasV) {
                 texto = br.readLine();
                 datos = texto.split(" ");
                 for (int i = 1; i < datos.length; i++) {
                     matriz[0][comisariasV] = Integer.parseInt(datos[i]);
                     ++comisariasV;
-                    //System.out.print("Y vale: " + x + "  comisariasV vale: " + comisariasV + " \n");
                 }
             }
-            //System.out.print(texto = br.readLine()+"  SOBROOOOOOOOO\n"); //<---------------------- SOBRA URGENTEMENTE
 
-            /*
-            for (int i = 0; i < y; i++) {
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-            //System.out.print("X vale: " + y + " \n");
             int cont;
             for (int i = 1; i < y; i++) {
-                //System.out.print("Leyendo las colindancias del territorio " + i + " \n");
                 texto = br.readLine();
                 datos = texto.split(" ");
                 cont = Integer.parseInt(datos[1]);
-                //System.out.print("El territorio " + i + " tiene " + cont + " colindancias. \n");
                 while (cont != 0) {
                     texto = br.readLine();
                     datos = texto.split(" ");
@@ -112,20 +88,6 @@ public class Pr1Metah {
                 }
             }
 
-            //System.out.print(Arrays.toString(datos)+"aaaaaaaaaaa \n");
-            /*
-            for (int i = 0; i < y; i++) {    //SOBRA
-                for (int j = 0; j < x; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.print("\n");
-            }*/
-
- /*
-            while ((texto = br.readLine()) != null) {
-                System.out.print(texto + "\n");
-            }
-             */
         } catch (IOException | NumberFormatException e) {
         } finally {
             try {
@@ -137,9 +99,9 @@ public class Pr1Metah {
             }
         }
     }
-    
-    public static void busquedaLocal (int solucionGreedy[], int tam){
-        
+
+    public static void busquedaLocal(int solucionGreedy[], int tam) {
+
     }
 
     public static void main(String[] args) {
