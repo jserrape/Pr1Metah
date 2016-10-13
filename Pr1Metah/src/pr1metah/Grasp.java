f32798b1ea236dfc0229ef1ec0856dd9dc545226
xenahort <jcsp0003@red.ujaen.es>
2016-10-13 20:25:21 +0200

    -Haciendo el Grasp 	+GreedyRandomiced funciona, pero al llamarlo una segunda vez da 0 wtf

--- Pr1Metah/src/pr1metah/Grasp.java
+++ Pr1Metah/src/pr1metah/Grasp.java
@@ -15,24 +15,49 @@ public class Grasp {
 
     float alpha;
 
-    public void mostrarMatriz(int x, int y, int mat[][]) {
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                System.out.print(mat[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-    }
-
-    public void graspSearch(int x, int y, int mat[][]) { //LA MATRIZ LA MACHACO!!!
+    public void graspSearch(int x, int y, int mat[][]) {
+        int solAux[], mejorSol[] = new int[x];
+        int mejorCoste = 9999999, cosAux;
         System.out.println("\n\nComienzo la busqueda Grasp:");
         System.out.println("   x=" + x);
         System.out.println("   y=" + y);
-        alpha = (float) 0.6;
+        alpha = (float) 0.7;
         System.out.println("   alpha=" + alpha);
 
-        mostrarMatriz(x,y,mat);
-        greedyRandomized(x, y, mat.clone());
+        //mostrarMatriz(x, y, mat);
+
+        for (int i = 1; i < 3; i++) {
+            solAux = greedyRandomized(x, y, mat);
+            //Busqueda local a solAux aqui
+            cosAux = costeSol(x, solAux, mat);
+            System.out.println("Coste vuelta "+i+": "+cosAux);
+            System.out.println(cosAux +" < "+ mejorCoste);
+            if (cosAux < mejorCoste) {
+                mejorCoste = cosAux;
+                mejorSol = solAux.clone();
+            }
+        }
+        System.out.println("Grasp, resultado de coste: " + mejorCoste);
+
+    }
+
+    public int costeSol(int x, int sol[], int mat[][]) {
+        int cos = 0;
+        for (int i = 1; i < x; i++) {
+            if (sol[i] == 1) {
+                cos += mat[0][i];
+            }
+        }
+        return cos;
+    }
+
+    public void mostrarMatriz(int x, int y, int mat[][]) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                //System.out.print(mat[i][j] + " ");
+            }
+            //System.out.print("\n");
+        }
     }
 
     public int actualizaCubre(int x, int y, int mat[][], int cubre[]) {
@@ -47,15 +72,15 @@ public class Grasp {
             }
         }
         int mayor = cubre[1];
-        System.out.println("   cubre:");
+        //System.out.println("   cubre:");
         for (int i = 1; i < x; i++) {
-            System.out.print(cubre[i] + " ");
+            //System.out.print(cubre[i] + " ");
             if (mayor < cubre[i]) {
                 mayor = cubre[i];
             }
         }
-        System.out.print("\n");
-        System.out.print("   NObjNoCub=" + mayor + "\n");
+        //System.out.print("\n");
+        //System.out.print("   NObjNoCub=" + mayor + "\n");
         return mayor;
     }
 
@@ -75,24 +100,29 @@ public class Grasp {
                 return true;
             }
         }
-        return true;
+        return false;
     }
 
-    public void greedyRandomized(int x, int y, int mat[][]) {
+    public int[] greedyRandomized(int x, int y, int mat[][]) {
         int cubre[] = new int[x];
         int sol[] = new int[x];
         int lrc[] = new int[x];
-        int NObjNoCub, tam = 0, nRand;
+        int NObjNoCub, tam = 0, nRand, aux;
         float umbral;
         Random rand = new Random();
         for (int i = 1; i < x; i++) {
             sol[i] = 0;
         }
 
+        NObjNoCub = actualizaCubre(x, y, mat, cubre);
         while (faltaPorCubir(x, cubre)) {
-            NObjNoCub = actualizaCubre(x, y, mat, cubre);
+            //System.out.println("   sol:");
+            for (int i = 1; i < x; i++) {
+                //System.out.print(sol[i] + " ");
+            }
+            //System.out.print("\n");
             umbral = (float) alpha * NObjNoCub;
-            System.out.println("   umbral:" + umbral);
+            //System.out.println("   umbral:" + umbral);
             tam = 0;
             for (int i = 1; i < x; i++) {
                 if (cubre[i] >= umbral) {
@@ -100,16 +130,29 @@ public class Grasp {
                     ++tam;
                 }
             }
-            System.out.println("   lrc:");
+            //System.out.println("   lrc:");
             for (int i = 0; i < tam; i++) {
-                System.out.print(lrc[i] + " ");
+                //System.out.print(lrc[i] + " ");
             }
-            System.out.print("\n");
+
+            //System.out.print("\n");
             nRand = (int) (rand.nextDouble() * tam);
-            System.out.print("   nRand:\n" + nRand + "\n");
-            ++sol[nRand]; //Lo pongo como solucion
-            cubrir(x, y, mat, nRand);
-            System.out.println("Fin de la vuelta");
+            aux = lrc[nRand];
+            //System.out.print("   nRand:\n" + nRand + "\n");
+            //System.out.print("   lrc[nRand]:\n" + lrc[nRand] + "\n");
+            ++sol[aux]; //Lo pongo como solucion
+            cubrir(x, y, mat, aux);
+            //System.out.println("Fin de la vuelta \n\n\n\n");
+            NObjNoCub = actualizaCubre(x, y, mat, cubre);
         }
+
+        //System.out.println("   solucion final:");
+        for (int i = 1; i < x; i++) {
+            //System.out.print(sol[i] + " ");
+        }
+        //System.out.println();
+        int cosAux = costeSol(x, sol, mat);
+        System.out.println("Resultado del greedy: "+cosAux);
+        return sol;
     }
 }
--- Pr1Metah/src/pr1metah/Pr1Metah.java
+++ Pr1Metah/src/pr1metah/Pr1Metah.java
@@ -97,16 +97,16 @@ public class Pr1Metah {
         Greedy greedy = new Greedy();
         Grasp grasp = new Grasp();
 
-        String ficheros[] = {"scpe1.txt", "scp41.txt", "scpd1.txt", "scpnrf1.txt", "scpnrh4.txt"}; //El ultimo fichero esta dañado
+        String ficheros[] = {"scpe1.txt", "scp41.txt", "scpd1.txt", "scpnrf1.txt", "scpa1.txt"}; 
         int n = 4;
 
         greedy = new Greedy();
-        leerFichero(ficheros[0]);
+        leerFichero(ficheros[4]);
         //int solucion[] = greedy.greedySearch(x, y, matriz, cubre, pa, ficheros[0], 0);
 
         
-        x=11;
-        y=21;
+        //x=11;
+        //y=21;
         grasp.graspSearch(x, y, matriz);
 
     }
